9d3fc3d6a4b3ca6706daa55af734edcc2a0b8d4b
goalista10 <agriam_angelo@yahoo.com.ph>
2021-05-08 21:40:10 +0800

    final

--- bakery.rb
+++ bakery.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
+
 require './bakery_functions'
 
+# add another case when adding bread
 def determine_bread(code)
-  vegemite_scroll = Bread.new("VS5",[5,3],[8.99, 6.99])
-  blueberry_muffin = Bread.new("MB11",[8, 5, 2],[24.95, 16.95, 9.95])
-  croissant = Bread.new("CF",[9, 5, 3],[16.99, 9.95, 5.95])
-  
-  number = code.match(/\d+/).to_s.to_i
   case code
-  when /#{vegemite_scroll.code}/
-    variables = vegemite_scroll.variables
-    price = vegemite_scroll.price
-  when /#{blueberry_muffin.code}/
-    variables = blueberry_muffin.variables
-    price = blueberry_muffin.price
-  when /#{croissant.code}/
-    variables = croissant.variables
-    price = croissant.price
+  when /VS5/
+    variables = [5, 3]
+    price = [8.99, 6.99]
+  when /MB11/
+    variables = [8, 5, 2]
+    price = [24.95, 16.95, 9.95]
+  when /CF/
+    variables = [9, 5, 3]
+    price = [16.99, 9.95, 5.95]
   else
     raise InvalidBreadCode
   end
-  solution = Array.new(variables.length, 0)
-  total = print_receipt(variables, price, calculate(number, variables, solution, -1, number), code)
-  [calculate(number, variables, solution, -1, number), variables, total]
+  [variables, price]
 end
--- bakery_functions.rb
+++ bakery_functions.rb
@@ -1,48 +1,50 @@
-def multiply_array(variables, solution)
-    sum = 0
-    (0..variables.length - 1).each do |x|
-      sum += (variables[x] * solution[x])
-    end
-    sum
-  end
-  
-  def calculate(number, variables, solution, ctr, orig_number)
-    if ctr != variables.length - 1
-      ctr += 1
-      a = variables[ctr]
-      r = (number / a)..0
-      (r.first).downto(r.last).each do |x|
-        solution[ctr] = x
-        ret = calculate(number - (a * x), variables, solution, ctr, orig_number)
-        return ret if ret.instance_of?(Array)
-      end
-    elsif (multiply_array(variables, solution)) == orig_number
-      solution
-    end
-  end
-  
-  def print_receipt(variables, price, solution, code)
-    puts ''
-    puts "#{code}, Your total is $#{(multiply_array(price, solution) * 100).round / 100.0}"
-    puts('BREAKDOWN')
-    (0..solution.length - 1).each do |x|
-      puts "#{solution[x]} x #{variables[x]} pack = $#{(solution[x] * price[x] * 100).round / 100.0}"
-    end
-    (multiply_array(price, solution) * 100).round / 100.0
-  end
-  
-  class Bread
-    attr_reader :code, :variables, :price
-    def initialize(code, variables, price)
-      @code = code
-      @variables = variables
-      @price = price
-    end
-  end
+# frozen_string_literal: true
 
-  class InvalidBreadCode < StandardError
-    def initialize(msg="The code you entered is invalid", exception_type="custom")
-      @exception_type = exception_type
-      super(msg)
-    end
+def multiply_array(variables, solution)
+  sum = 0
+  (0..variables.length - 1).each do |x|
+    sum += (variables[x] * solution[x])
   end
+  sum
+end
+
+def calculate(number, variables, solution, ctr, orig_number)
+  if ctr != variables.length - 1
+    ctr += 1
+    a = variables[ctr]
+    r = (number / a)..0
+    (r.first).downto(r.last).each do |x|
+      solution[ctr] = x
+      ret = calculate(number - (a * x), variables, solution, ctr, orig_number)
+      return ret if ret.instance_of?(Array)
+    end
+  elsif (multiply_array(variables, solution)) == orig_number
+    solution
+  end
+end
+
+def print_receipt(variables, price, solution, code)
+  puts ''
+  puts "#{code}, Your total is $#{(multiply_array(price, solution) * 100).round / 100.0}"
+  puts('BREAKDOWN')
+  (0..solution.length - 1).each do |x|
+    puts "#{solution[x]} x #{variables[x]} pack = $#{(solution[x] * price[x] * 100).round / 100.0}"
+  end
+  (multiply_array(price, solution) * 100).round / 100.0
+end
+
+# Custom error for invalid code
+class InvalidBreadCode < StandardError
+  def initialize(msg = 'The code you entered is invalid', exception_type = 'custom')
+    @exception_type = exception_type
+    super(msg)
+  end
+end
+
+def decode(code)
+  number = code.match(/\d+/).to_s.to_i
+  variables, price = determine_bread(code)
+  solution = Array.new(variables.length, 0)
+  total = print_receipt(variables, price, calculate(number, variables, solution, -1, number), code)
+  [calculate(number, variables, solution, -1, number), variables, total]
+end
