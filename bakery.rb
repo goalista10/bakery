00abfeab19898d22e58555e75014cc9d94b09658
goalista10 <agriam_angelo@yahoo.com.ph>
2021-05-08 12:49:43 +0800

    rubocop autocorrect

--- bakery.rb
+++ bakery.rb
@@ -1,54 +1,52 @@
-def multiply_array(var,sol)
+# frozen_string_literal: true
+
+def multiply_array(var, sol)
   sum = 0
-  (0..var.length-1).each do |x|
-    sum = sum + (var[x] * sol[x])
+  (0..var.length - 1).each do |x|
+    sum += (var[x] * sol[x])
   end
-  return sum
+  sum
 end
 
-def calculate(num,var,sol,ctr,orig_num)
-  if ctr!=var.length-1
-    ctr+=1
+def calculate(num, var, sol, ctr, orig_num)
+  if ctr != var.length - 1
+    ctr += 1
     a = var[ctr]
-    r = (num/a)..0
+    r = (num / a)..0
     (r.first).downto(r.last).each do |x|
       sol[ctr] = x
-      ret = calculate(num-(a*x),var,sol,ctr,orig_num)
-      if ret.class == Array
-        return ret
-      end
+      ret = calculate(num - (a * x), var, sol, ctr, orig_num)
+      return ret if ret.instance_of?(Array)
     end
-  else
-    if (multiply_array(var,sol)) == orig_num
-      return sol
-    end
+  elsif (multiply_array(var, sol)) == orig_num
+    sol
   end
 end
 
-def print_receipt(var,price,sol,code)
-  puts ""
-  puts "#{code}, Your total is $#{(multiply_array(price,sol)*100).round / 100.0}"
-  puts ("BREAKDOWN")
-  (0..sol.length-1).each do |x|
-    puts "#{sol[x]} x #{var[x]} pack = $#{(sol[x]*price[x]*100).round / 100.0}"
+def print_receipt(var, price, sol, code)
+  puts ''
+  puts "#{code}, Your total is $#{(multiply_array(price, sol) * 100).round / 100.0}"
+  puts('BREAKDOWN')
+  (0..sol.length - 1).each do |x|
+    puts "#{sol[x]} x #{var[x]} pack = $#{(sol[x] * price[x] * 100).round / 100.0}"
   end
-  return (multiply_array(price,sol)*100).round / 100.0
+  (multiply_array(price, sol) * 100).round / 100.0
 end
 
 def determine_bread(code)
   num = code.match(/\d+/).to_s.to_i
-  if code.match(/VS5/)
-    var = [5,3]
-    price = [8.99,6.99]   
-  elsif code.match(/MB11/)
-    var = [8,5,2]
-    price = [24.95,16.95,9.95]
-  elsif code.match(/CF/)
-    var = [9,5,3]
-    price = [16.95,9.95,5.95]
+  case code
+  when /VS5/
+    var = [5, 3]
+    price = [8.99, 6.99]
+  when /MB11/
+    var = [8, 5, 2]
+    price = [24.95, 16.95, 9.95]
+  when /CF/
+    var = [9, 5, 3]
+    price = [16.95, 9.95, 5.95]
   end
-  sol = Array.new(var.length,0)
-  total = print_receipt(var,price,calculate(num,var,sol,-1,num),code)
-  return calculate(num,var,sol,-1,num),var,total
+  sol = Array.new(var.length, 0)
+  total = print_receipt(var, price, calculate(num, var, sol, -1, num), code)
+  [calculate(num, var, sol, -1, num), var, total]
 end
-
--- bakery_spec.rb
+++ bakery_spec.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 require_relative './bakery'
 
-RSpec.describe "Bakery" do
-    it "should determine the bread vegemite scroll, provide the appropriate solution, and total" do
-        bread = determine_bread("10 VS5")
-        expect(bread[0]).to eq([2,0])
-        expect(bread[1]).to eq([5,3])
-        expect(bread[2]).to eq(17.98)
-      end
+RSpec.describe 'Bakery' do
+  it 'should determine the bread vegemite scroll, provide the appropriate solution, and total' do
+    bread = determine_bread('10 VS5')
+    expect(bread[0]).to eq([2, 0])
+    expect(bread[1]).to eq([5, 3])
+    expect(bread[2]).to eq(17.98)
+  end
 
-      it "should determine the bread blueberry muffin, provide the appropriate solution, and total" do
-        bread = determine_bread("14 MB11")
-        expect(bread[0]).to eq([1,0,3])
-        expect(bread[1]).to eq([8,5,2])
-        expect(bread[2]).to eq(54.8)
-      end
-      it "should determine the bread croissant, provide the appropriate solution, and total" do
-        bread = determine_bread("13 CF")
-        expect(bread[0]).to eq([0,2,1])
-        expect(bread[1]).to eq([9,5,3])
-        expect(bread[2]).to eq(25.85)
-      end
+  it 'should determine the bread blueberry muffin, provide the appropriate solution, and total' do
+    bread = determine_bread('14 MB11')
+    expect(bread[0]).to eq([1, 0, 3])
+    expect(bread[1]).to eq([8, 5, 2])
+    expect(bread[2]).to eq(54.8)
+  end
+  it 'should determine the bread croissant, provide the appropriate solution, and total' do
+    bread = determine_bread('13 CF')
+    expect(bread[0]).to eq([0, 2, 1])
+    expect(bread[1]).to eq([9, 5, 3])
+    expect(bread[2]).to eq(25.85)
+  end
 end
-
-
