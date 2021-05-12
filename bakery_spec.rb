a84eaab94f500a3c85b9fdc6b7f9f4398c64ded5
goalista10 <agriam_angelo@yahoo.com.ph>
2021-05-12 19:25:53 +0800

    specifiy variables

--- bakery_spec.rb
+++ bakery_spec.rb
@@ -4,23 +4,23 @@ require_relative './bakery'
 
 RSpec.describe 'Bakery' do
   it 'should determine the bread vegemite scroll, provide the appropriate solution, and total' do
-    bread = decode('10 VS5')
-    expect(bread[0]).to eq([2, 0])
-    expect(bread[1]).to eq([5, 3])
-    expect(bread[2]).to eq(17.98)
+    solution, variables, total = decode('10 VS5')
+    expect(solution).to eq([2, 0])
+    expect(variables).to eq([5, 3])
+    expect(total).to eq(17.98)
   end
 
   it 'should determine the bread blueberry muffin, provide the appropriate solution, and total' do
-    bread = decode('14 MB11')
-    expect(bread[0]).to eq([1, 0, 3])
-    expect(bread[1]).to eq([8, 5, 2])
-    expect(bread[2]).to eq(54.8)
+    solution, variables, total = decode('14 MB11')
+    expect(solution).to eq([1, 0, 3])
+    expect(variables).to eq([8, 5, 2])
+    expect(total).to eq(54.8)
   end
   it 'should determine the bread croissant, provide the appropriate solution, and total' do
-    bread = decode('13 CF')
-    expect(bread[0]).to eq([0, 2, 1])
-    expect(bread[1]).to eq([9, 5, 3])
-    expect(bread[2]).to eq(25.85)
+    solution, variables, total = decode('13 CF')
+    expect(solution).to eq([0, 2, 1])
+    expect(variables).to eq([9, 5, 3])
+    expect(total).to eq(25.85)
   end
 
   it 'should raise error for invalid amount' do
